7f22d6b04e72711dca20cc62a37e76960ba67ab2
Toma Kazanskaya <tkazanskaya888@gmail.com>
2024-02-28 01:25:33 +0300

    добавляет разметку, переменные, шрифты шапку

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,22 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.navigation');
+const navBurger = document.querySelector('.burger');
+
+navBurger.addEventListener('click',
+  function () {
+  if (navMain.classList.contains('navigation--closed')) {
+    navMain.classList.remove('navigation--closed');
+    navMain.classList.add('navigation--opened');
+  } else {
+    navMain.classList.add('navigation--closed');
+    navMain.classList.remove('navigation--opened');
+  }
+
+  if (navBurger.classList.contains('burger--closed')) {
+    navBurger.classList.remove('burger--closed');
+    navBurger.classList.add('burger--opened');
+  } else {
+    navBurger.classList.add('burger--closed');
+    navBurger.classList.remove('burger--opened');
+  }
+});
